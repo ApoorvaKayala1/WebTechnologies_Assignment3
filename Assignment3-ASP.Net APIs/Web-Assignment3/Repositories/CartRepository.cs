44b3a6d80df85f16cff8ac474e8139b7b07b60f0
Apoorva Kayala <apoorva.kayala3012@gmail.com>
2024-03-29 23:42:45 +0530

    Commit - ASP.Net APIs

---
+++ Assignment3-ASP.Net APIs/Web-Assignment3/Repositories/CartRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Web_Assignment3.Models;
+
+namespace Web_Assignment3.Repositories
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CartRepository(AppDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IEnumerable<Cart> GetAllItems()
+        {
+            return _context.Cart.ToList();
+        }
+
+        public Cart GetItemById(int id)
+        {
+            return _context.Cart.Find(id);
+        }
+
+        public void AddItem(Cart item)
+        {
+            _context.Cart.Add(item);
+            _context.SaveChanges();
+        }
+
+        public void UpdateItem(Cart item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void DeleteItem(int id)
+        {
+            var item = _context.Cart.Find(id);
+            if (item != null)
+            {
+                _context.Cart.Remove(item);
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteAllItems()
+        {
+            try
+            {
+                _context.Cart.RemoveRange(_context.Cart); // Remove all items from the Cart table
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error deleting all items from the cart.", ex);
+            }
+        }
+
+        public IEnumerable<CartWithProductInfo> GetAllItemsWithProductInfo()
+        {
+            return _context.Cart
+                .Join(_context.Products,
+                    cartItem => cartItem.Product_Id,
+                    product => product.Id,
+                    (cartItem, product) => new CartWithProductInfo
+                    {
+                        Id = cartItem.Id,
+                        UserId = cartItem.User_Id, 
+                        ProductId = cartItem.Product_Id,
+                        Quantity = cartItem.Quantity,
+                        ProductName = product.Name,
+                        ProductImage = product.Image,
+                        ProductPricing = product.Pricing
+                    })
+                .ToList();
+        }
+    }
+}
